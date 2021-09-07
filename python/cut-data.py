bc46245a613fa18ada0b40795c7a81de74ac794f
bay <68962000+bay-puz@users.noreply.github.com>
2021-09-07 22:56:07 +0900

    データ加工

---
+++ python/cut-data.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*- #
+import json
+import argparse
+
+
+def cut_data(data: list):
+    author_dict = {}
+    puzzle_dict = {}
+    for d in data:
+        if d["author_name"] not in author_dict:
+            author = {"id": d["author_id"], "liked": 0, "problem": 0, "puzzle": []}
+            author_dict[d["author_name"]] = author
+        author_dict[d["author_name"]]["liked"] += d["liked"]
+        author_dict[d["author_name"]]["problem"] += 1
+        if d["puzzle_name"] not in author_dict[d["author_name"]]["puzzle"]:
+            author_dict[d["author_name"]]["puzzle"].append(d["puzzle_name"])
+
+        if d["puzzle_name"] not in puzzle_dict:
+            puzzle = {"id": d["puzzle_id"], "liked": 0, "problem": 0, "author": []}
+            puzzle_dict[d["puzzle_name"]] = puzzle
+        puzzle_dict[d["puzzle_name"]]["liked"] += d["liked"]
+        puzzle_dict[d["puzzle_name"]]["problem"] += 1
+        if d["author_name"] not in puzzle_dict[d["puzzle_name"]]["author"]:
+            puzzle_dict[d["puzzle_name"]]["author"].append(d["author_name"])
+
+    return author_dict, puzzle_dict
+
+
+def load(file: str):
+    with open(file, encoding='utf8') as f:
+        data_json = f.read()
+        return json.loads(data_json)
+
+
+def write(data: dict, file: str):
+    with open(file, 'w', encoding="utf8") as f:
+        f.write(json.dumps(data, ensure_ascii=False, indent=2))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='PSJPの問題毎のデータを作者毎・パズル毎に変換する')
+    parser.add_argument("--input", type=str, default="../data/data.json", help="問題別データの書かれたファイル")
+    parser.add_argument("--authors", type=str, default="../data/authors.json", help="作者別データの出力先")
+    parser.add_argument("--puzzles", type=str, default="../data/puzzles.json", help="パズル別データの出力先")
+    args = parser.parse_args()
+
+    data = load(args.input)
+    authors, puzzles = cut_data(data)
+
+    write(authors, args.authors)
+    write(puzzles, args.puzzles)
+
+    return
+
+if __name__ == '__main__':
+    main()
