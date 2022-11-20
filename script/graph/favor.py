37a6936d79fb619165a849a8fe7df26e5d9629b7
bay <68962000+bay-puz@users.noreply.github.com>
2022-11-20 17:51:01 +0900

    解答登録数をグラフに反映

---
+++ script/graph/favor.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*- #
+import json
+from matplotlib import pyplot
+
+
+def load(file: str):
+    with open(file, encoding='utf8') as f:
+        data_json = f.read()
+        return json.loads(data_json)
+
+
+def get_fav_ans(data: dict, difficulty: int):
+    fav_list = []
+    ans_list = []
+    fav_sum = 0
+    ans_sum = 0
+    for d in data.values():
+        if d["difficulty"] != difficulty:
+            continue
+        fav_list.append(d["favorite_n"])
+        ans_list.append(d["answered_n"])
+        fav_sum += d["favorite_n"]
+        ans_sum += d["answered_n"]
+    return fav_list, ans_list
+
+
+def plot(data):
+    pyplot.rcParams["font.family"] = 'MotoyaLMaru'
+    fig = pyplot.figure()
+    pyplot.xlabel("解答登録数")
+    pyplot.ylabel("いいね数")
+    pyplot.title("問題別解答登録数/いいね数")
+
+    difficultys = [{"n": 1, "s": "らくらく", "c": "b", "m": "s"}, {"n": 2, "s": "おてごろ", "c": "g", "m": "s"}, {"n": 3, "s": "たいへん", "c": "y", "m": "s"}, {"n": 4, "s": "アゼン", "c": "orange", "m": "x"}, {"n": 5, "s": "ハバネロ", "c": "r", "m": "x"}]
+    for d in difficultys:
+        fav, ans = get_fav_ans(data, d["n"])
+        pyplot.scatter(ans, fav, s=1, c=d["c"], marker=d["m"], alpha=0.5)
+    pyplot.legend([d["s"] for d in difficultys], loc="upper left")
+    fig.savefig(f"graph/answered-favorite.png")
+
+
+def main():
+    data = load("data/data.json")
+    plot(data)
+
+
+if __name__ == '__main__':
+    main()
--- script/graph/problem.py
+++ script/graph/problem.py
@@ -9,34 +9,43 @@ def load(file: str):
         return json.loads(data_json)
 
 
-def problem_favorite(data_dict: dict):
+def count_problem(data_dict: dict):
     problem_list = []
     favorite_list = []
+    answered_list = []
     for data in data_dict.values():
         problem_list.append(data["problem_n"])
         favorite_list.append(data["favorite_n"])
-    return problem_list, favorite_list
+        answered_list.append(data["answered_n"])
+    return problem_list, favorite_list, answered_list
 
 
-def plot_problem_favorite(author_dict, puzzle_dict):
+def plot(x, y, is_author, is_fav):
+    category = "作者" if is_author else "パズル"
+    label = "いいね" if is_fav else "被解答登録"
     pyplot.rcParams["font.family"] = 'MotoyaLMaru'
     fig = pyplot.figure()
 
     pyplot.xlabel("問題数")
-    pyplot.ylabel("いいね数")
+    pyplot.ylabel(f"{label}数")
     pyplot.xscale("log")
     pyplot.yscale("log")
 
-    x, y = problem_favorite(author_dict)
-    pyplot.title("作者別問題数/いいね数")
+    pyplot.title(f"{category}別問題数/{label}数")
     pyplot.scatter(x, y, s=5, c='g')
-    fig.savefig("graph/problem-favorite-author.png")
+    category_en = "author" if is_author else "puzzle"
+    label_en = "favorite" if is_fav else "answered"
+    fig.savefig(f"graph/problem-{label_en}-{category_en}.png")
 
-    x, y = problem_favorite(puzzle_dict)
-    pyplot.title("パズル別問題数/いいね数")
-    pyplot.scatter(x, y, s=5, c='g')
-    fig.savefig("graph/problem-favorite-puzzle.png")
-    return
+
+def plot_problem(author_dict, puzzle_dict):
+    p, f, a = count_problem(author_dict)
+    plot(p, f, is_author=True, is_fav=True)
+    plot(p, a, is_author=True, is_fav=False)
+
+    p, f, a = count_problem(puzzle_dict)
+    plot(p, f, is_author=False, is_fav=True)
+    plot(p, a, is_author=False, is_fav=False)
 
 
 def top_in_problem(data_dict: dict, name_dict: dict, type: str, limit: int):
@@ -113,7 +122,7 @@ def main():
     del data_author["0"]
     del data_puzzle["0"]
 
-    plot_problem_favorite(data_author, data_puzzle)
+    plot_problem(data_author, data_puzzle)
     plot_top_in_problem(data_author, data_puzzle, data_user, data_kind)
 
 
