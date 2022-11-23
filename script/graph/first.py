7ede70baf3e70db187c7f3bf31c804fa03b8d4a9
bay <68962000+bay-puz@users.noreply.github.com>
2022-11-23 19:13:05 +0900

    表示するグラフを減らす

--- script/graph/first.py
+++ script/graph/first.py
@@ -63,59 +63,10 @@ def plot_first_categories(data: dict, is_author: bool, name_dict: dict):
     fig.savefig("graph/" + image_name)
 
 
-def get_first_month(data: dict, is_author: bool, name_dict: dict):
-    search_id = "user" if is_author else "kind"
-    first_problem = {}
-    for d in data.values():
-        category_id = d[search_id]
-        time = datetime.fromisoformat(d["registered"]).strftime("%Y-%m")
-        name = name_dict[str(category_id)]["name"]
-        if name in first_problem:
-            if first_problem[name] > time:
-                first_problem[name] = time
-        else:
-            first_problem[name] = time
-
-    times = []
-    count = []
-    for name, time in first_problem.items():
-        if time in times:
-            count[times.index(time)] += 1
-        else:
-            times.append(time)
-            count.append(1)
-
-    return times, count
-
-
-def plot_first_month(data: dict, is_author: bool, name_dict: dict):
-    type_display = "作者" if is_author else "パズル"
-    image_name = "first-month-author.png" if is_author else "first-month-puzzle.png"
-
-    pyplot.rcParams["font.family"] = 'MotoyaLMaru'
-    fig = pyplot.figure(figsize=[14, 7])
-    ax = pyplot.axes()
-    ax.set_axisbelow(True)
-    ax.tick_params(width=0, which="both")
-    pyplot.subplot().margins(0.01, 0.05)
-    pyplot.grid(axis="y", which="both", c="grey")
-    pyplot.xticks(rotation=50)
-
-    pyplot.title("初投稿の" + type_display + "の数")
-    pyplot.xlabel("")
-    pyplot.ylabel(type_display + "数")
-
-    x, y = get_first_month(data, is_author, name_dict)
-    pyplot.plot(x, y, color="green")
-    fig.savefig("graph/" + image_name)
-
-
 def main():
     data = load("data/data.json")
     user_data = load("data/user.json")
     kind_data = load("data/kind.json")
-    plot_first_month(data, True, user_data)
-    plot_first_month(data, False, kind_data)
 
     plot_first_categories(data, True, user_data)
     plot_first_categories(data, False, kind_data)
