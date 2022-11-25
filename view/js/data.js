bca5d5d40326145f3765d786582b5b8746db14d9
bay <68962000+bay-puz@users.noreply.github.com>
2022-11-25 18:27:53 +0900

    全作者・全パズルを表示する

--- view/js/data.js
+++ view/js/data.js
@@ -50,6 +50,12 @@ async function getNameData(type) {
 }
 
 async function getIdByName(name, type) {
+    if ( (type === "kind" || type === "puzzle" ) && name === allPuzzle["name"]) {
+        return allPuzzle["id"]
+    }
+    if ( (type === "user" || type === "author" ) && name === allAuthor["name"]) {
+        return allAuthor["id"]
+    }
     data = await getNameData(type)
     for (const key in data) {
         if (data[key].name === name) {
@@ -61,6 +67,12 @@ async function getIdByName(name, type) {
 
 // 高速化のためファイルのfetchをしない
 function getNameById(id, type, userData, kindData) {
+    if (id === 0) {
+        if (type === "kind" || type === "puzzle" ) {
+            return allPuzzle["name"]
+        }
+        return allAuthor["name"]
+    }
     if ( type === "kind"  || type === "puzzle" ){
         data = kindData
     } else if ( type === "user" || type === "author" ){
@@ -87,6 +99,8 @@ function initData() {
     return {"name": "", "problem_n": 0, "favorite_n":0, "answered_n": 0, "variant_n":0, "count": 0, "puzzle": {}, "author": {}, "difficulty": dif}
 }
 
+const allPuzzle = {"name": "全パズル", "id": 0}
+const allAuthor = {"name": "全作者", "id": 0}
 const displayStr = {"kind": "パズル", "author": "作者", "favorite_n": "いいね数", "answered_n": "解答者数", "problem_n": "問題数", "problem_r": "占有率", "kind_c": "作者数",  "author_c": "パズル数", "favorite_r": "平均いいね数", "answered_r": "平均解答者数", "kind_r": "作者平均", "author_r": "パズル平均", "variant_n": "変種数", "variant_r": "変種率", "difficulty": "難易度"}
 const displayCountStr = {"kind": "投稿したパズルの種類", "author": "投稿した作者の人数", "kind_r": "１種類当たり問題数", "author_r": "１人当たり問題数"}
 const displayDifficultyStr = {1: "らくらく", 2: "おてごろ", 3: "たいへん", 4: "アゼン", 5: "ハバネロ"}
