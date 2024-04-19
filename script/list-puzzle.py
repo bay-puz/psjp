7342df67cee82a6fff357a14d7365d77c3b500c6
bay <68962000+bay-puz@users.noreply.github.com>
2024-04-19 22:38:31 +0900

    パズルのリストに問題数も載せる

--- script/list-puzzle.py
+++ script/list-puzzle.py
@@ -2,35 +2,44 @@
 import json
 
 
-def cut_data(pid, data):
-    name = ""
-    if data["name"] != "":
-        name += data["name"]
-        if data["name_e"] != "" and data["name"] != data["name_e"]:
-            name += " (" + data["name_e"] + ")"
-    else:
-        name += data["name_e"]
-
+def markdown(p_dict: dict):
+    pid = p_dict["pid"]
+    name = p_dict["name"]
+    count = p_dict["count"]
     url = f"https://puzsq.logicpuzzle.app/?kind={pid}"
-
-    return f"1. {name} [🔗]({url})"
+    return f"1. [{name}]({url}) ({count})"
 
 
 def main():
     kind = {}
+    with open("data/kind.json", encoding='utf8') as fk:
+        kind = json.load(fk)
 
-    with open("data/kind.json", encoding='utf8') as fi:
-        kind = json.load(fi)
+    puzzle = {}
+    with open("data/puzzle.json", encoding='utf8') as fp:
+        puzzle = json.load(fp)
 
-    puzzles = []
-    for pid, data in kind.items():
-        puzzles.append(cut_data(pid, data))
-    list.sort(puzzles)
+    puzzles_dict = {}
+    for pid in kind:
+        p_dict = {"pid": pid, "name": "", "count": 0}
+        name_ja = kind[pid]["name"].strip()
+        name_en = kind[pid]["name_e"].strip()
+        if len(name_ja) > 0:
+            p_dict["name"] += name_ja
+            if len(name_en) > 0 and name_en != name_ja:
+                p_dict["name"] += " (" + name_en + ")"
+        else:
+            p_dict["name"] += name_en
+        if pid in puzzle:
+            p_dict["count"] = puzzle[pid]["problem_n"]
+        puzzles_dict[pid] = p_dict
+    puzzles = sorted(puzzles_dict.values(), key=lambda x: x["name"])
 
     with open("data/puzzle_list.md", "w") as fo:
-        fo.write("# ぱずすくのパズルのリスト\n")
-        for p in puzzles:
-            fo.write(p)
+        fo.write("# ぱずすくのパズルのリスト")
+        fo.write('\n\n')
+        for puzzle in puzzles:
+            fo.write(markdown(puzzle))
             fo.write('\n')
 
 
